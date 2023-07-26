7ccdc598d9cf2865a50205d8c98ce5a7a99d045f
Ronuhz <110656577+Ronuhz@users.noreply.github.com>
2023-07-26 09:40:33 +0300

    Standalone status bar fix

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -28,8 +28,8 @@ const MainNav = async () => {
 	const currentUser = await getCurrentUser()
 
 	return (
-		<header className='sticky top-0 z-20 standalone:h-80'>
-			<nav className=' flex w-full flex-row items-center justify-between px-6 pb-1 pt-2 backdrop-blur-lg sm:px-8'>
+		<header className='sticky top-0 z-20'>
+			<nav className='flex w-full flex-row items-center justify-between px-6 pb-1 pt-2 backdrop-blur-lg sm:px-8 standalone:items-end standalone:pt-20'>
 				<Link href='/'>
 					<h1 className='text-lg font-bold sm:text-2xl'>NEXT Social</h1>
 				</Link>
