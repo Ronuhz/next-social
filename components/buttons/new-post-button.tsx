ff293f03c62859c3518559bce7746785bf8ba0f0
Ronuhz <110656577+Ronuhz@users.noreply.github.com>
2023-07-26 19:21:33 +0300

    Type-o fix in new post button

--- components/buttons/new-post-button.tsx
+++ components/buttons/new-post-button.tsx
@@ -36,7 +36,7 @@ export const NewPostButton = () => {
 		content: z
 			.string()
 			.min(1, { message: 'Content must be at least 1 character long' })
-			.max(600, { message: 'Username must be shorter then 20 characters' }),
+			.max(600, { message: 'Content must be shorter then 600 characters' }),
 	})
 
 	const form = useForm<z.infer<typeof newPostFormSchema>>({
